d57eca311e019f7aa0faa1509bdf4b7518fd7a39
Tobias Grosse-Puppendahl <tobias@grosse-puppendahl.de>
2026-04-09 16:59:44 +0200

    fixing astro

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
+  site: "https://archmaxai.github.io",
+  base: "/archmax",
   integrations: [
     starlight({
       title: "archmax",
